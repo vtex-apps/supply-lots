45aea6278e0ba961a639a16bcabdb0e677f392f0
GabrielEluan <gabrieleluan9@gmail.com>
2021-06-11 17:49:13 -0300

    first stable version

--- node/__tests__/simple.test.ts
+++ node/__tests__/simple.test.ts
@@ -2,30 +2,30 @@
 //
 // More info: https://github.com/sapegin/jest-cheat-sheet
 
-import {setSupplyLot} from '../resolvers/supplyLots/index'
+// import {setSupplyLot} from '../resolvers/supplyLots/index'
 
-const supplyLotList = []
-const supplyLotData = {
+// const supplyLotList = []
+// const supplyLotData = {
 
-}
+// }
 
-const getContext = (supplyLotList: []): unknown => {
-  return {
-    clients: {
-      supplyLot: {
-        list: () => supplyLotList,
-        set: () => {}
-      }
-    }
-  }
+// const getContext = (supplyLotList: []): unknown => {
+//   return {
+//     clients: {
+//       supplyLot: {
+//         list: () => supplyLotList,
+//         set: () => {}
+//       }
+//     }
+//   }
 
-}
+// }
 
-describe(`teste 1`, () => {
-  it(`empty array`, async () => {
-    const ctx = getContext([]) as Context
-    //const result = await setSupplyLot('', supplyLotData, ctx)
-   // expect(result).toBe(true)
-  })
+// describe(`teste 1`, () => {
+//   it(`empty array`, async () => {
+//     const ctx = getContext([]) as Context
+//     const result = await setSupplyLot('', supplyLotData, ctx)
+//     expect(result).toBe(true)
+//   })
 
-})
+// })
